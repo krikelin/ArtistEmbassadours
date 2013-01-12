088d5e6d52571e56004993609b8cb6999362e966
Alexander Forselius <drsounds@gmail.com>
2013-01-12 14:42:54 +0100

    Added better admin

--- assets/js/bungalow.js
+++ assets/js/bungalow.js
@@ -131,198 +131,233 @@ embassadour
 		document.querySelector("#imgbox").innerHTML = "";
 		document.querySelector('#admin').style.display = 'none';
 		document.querySelector('#divider_admin').style.display = 'none';
+		document.querySelector('#admin #embassadours').innerHTML = '';
 
 	}
 	function userLoaded (user) {
 		cleanUI();
-		
-		require(['$views/image#Image'], function(Image) {
-		//	document.querySelector("#role").innerHTML = text;
-			var image = Image.fromSource(user.image, {
-			    width: 120,
-			    height: 120
-			  })
-			document.querySelector("#imgbox").appendChild(image.node);
-		});
-		console.log(user.artist);
-		if(user.artist != '') {
-			console.log('user', user);
-			var text = "artist";
-			document.querySelector("#embdetails").style.display = "block";
-			require(['$views/buttons'], function(buttons) {
-
-			  	var button = buttons.Button.withLabel('View artist profile');
-			  	button.setSize('small');
-			 	 button.addEventListener('click', function() {
-			    	self.location = user.artist.uri;
-			  	});
-			  document.querySelector("#role").appendChild(button.node);
-
+		require(['$api/models'], function(models) {
+			require(['$views/image#Image'], function(Image) {
+			//	document.querySelector("#role").innerHTML = text;
+				var image = Image.fromSource(user.image, {
+				    width: 120,
+				    height: 120
+				  })
+				document.querySelector("#imgbox").appendChild(image.node);
 			});
-			user.artist.load('name').done(function (artist) {
-				document.querySelector("#username").innerText = artist.name;
-			});
-			// Load embassadours
-			 context.EmbassadourCollection.forUser(user, function (collection) {
-			 	require(['$api/models'] , function (models) {
-				 	console.log(collection);
-				 	var embds = [];
-				 	collection.items.forEach(function (item) {
-				 		console.log(item);
-				 		embds.push(models.User.fromURI(item).load('name', 'image'));
+			console.log(user.artist);
 
-				 	});
-				 	var users = [];
-				 	models.Promise.join(embds).each(function (user) {
-				 		
-				 			users.push(user);
-				 	}).done(function (d) {
-				 		require(['$views/image#Image', '$views/popup#Popup'], function(Image, Popup) {
-				 			document.querySelector('#embdetails').style.display = 'block';
-					 		document.querySelector('#embdetails').innerHTML = '<b>Embassadours</b><ul id=\"embassadours\"></ul>';
-					 		new models.Session().user.load('username').done(function (user2) {
-				 				var isAdmin = false;
-				 				if(user.username === user2.username) {
-				 					alert('a');
-				 					isAdmin = true;
-				 					document.querySelector('#divider_admin').style.display = 'block';
-		
-				 					document.querySelector('#admin').style.display = 'block';
-				 				}
-				 			
-					 			users.forEach(function (user) {
-						 			console.log(user);
-						 			console.log(user.image);
-						 			var image = Image.fromSource(user.image, {
-						 				width: 40,
-						 				height: 40
-						 			});
-						 			image.node.style.cssFloat = 'right';
-						 			var popup = Popup.withText(user.username);
-						 			console.log(image.node);
-								    image.node.addEventListener('mouseover', showPopup, false);
-								    image.node.addEventListener('mouseout', hidePopup, false);
-								    function showPopup() {
-								    	// Set the text that is found in the target's data-tooltip attribute
-								   		 // This is just an example, and you can get the text from anywhere
-										// popup.setText(this.getAttribute('data-tooltip'));
+			// Hardcode hack for nomy
+			if(user.username == 'nomy') {
+				user.artist = models.Artist.fromURI('spotify:artist:20bAxKr0YrCvceZeLqs37e');
+			}
+			if(user.artist != '') {
+				console.log('user', user);
+				var text = "artist";
+				document.querySelector("#embdetails").style.display = "block";
+				require(['$views/buttons'], function(buttons) {
 
-									 	// Show the popup for this target element
-									    popup.showFor(this);
-								 	}
-								  	function hidePopup() {
-								  	  	popup.hide();
-								 	 }
-						 			document.querySelector('#embassadours').appendChild(image.node);
+				  	var button = buttons.Button.withLabel('View artist profile');
+				  	button.setSize('small');
+				 	 button.addEventListener('click', function() {
+				    	self.location = user.artist.uri;
+				  	});
+				  document.querySelector("#role").appendChild(button.node);
 
-						 			// add to admin
-						 			if (isAdmin) {
-						 				require(['$views/buttons'], function (buttons) {
-						 					console.log('f');
-						 					var item = document.createElement('article');
-						 					item.style.display = 'block';
-						 					var button = buttons.Button.withLabel('Increase reputation');
-						 					var image = Image.fromSource(user.image, {
-								 				width: 90,
-								 				height: 90,
+				});
+				user.artist.load('name').done(function (artist) {
+					document.querySelector("#username").innerText = artist.name;
+				});
+				// Load embassadours
+				 context.EmbassadourCollection.forUser(user, function (collection) {
+				 	require(['$api/models'] , function (models) {
+					 	console.log(collection);
+					 	var embds = [];
+					 	collection.items.forEach(function (item) {
+					 		console.log(item);
+					 		embds.push(models.User.fromURI(item).load('name', 'image'));
 
-								 			});
-								 			
-								 			var label = document.createElement('span');
-								 			image.node.style.display = 'inline';
-								 			
-								 			
-								 			item.appendChild(image.node);
-								 			item.appendChild(label)
-								 			var n = document.createElement('b');
-								 			n.innerText = user.username;
-								 			label.appendChild(n);
-								 			label.appendChild(document.createElement('br'))
-								 			label.appendChild(button.node);
-								 			item.appendChild(label);
-								 			item.style.width = '100%';
-								 			document.querySelector('#admin #embassadours').appendChild(item);
-								 			document.querySelector('#admin #embassadours').appendChild(document.createElement('br'));
-
-						 				});
-						 			}
-						 		});
-					 		});
 					 	});
-				 	}).fail(function (users) {
+					 	var users = [];
+					 	models.Promise.join(embds).each(function (user) {
+					 		
+					 			users.push(user);
+					 	}).done(function (d) {
+					 		require(['$views/image#Image', '$views/popup#Popup'], function(Image, Popup) {
+					 			document.querySelector('#embdetails').style.display = 'block';
+						 		document.querySelector('#embdetails').innerHTML = '<b>Embassadours</b><ul id=\"embassadours\"></ul>';
+						 		new models.Session().user.load('username').done(function (user2) {
+					 				var isAdmin = false;
+					 				if(user.username === user2.username) {
+					 					isAdmin = true;
+					 					document.querySelector('#divider_admin').style.display = 'block';
+			
+					 					document.querySelector('#admin').style.display = 'block';
 
-				 	});
+					 					// add buttons to buttonland
+
+					 				}
+					 			
+						 			users.forEach(function (user) {
+
+							 			console.log(user);
+							 			console.log(user.image);
+							 			var image = Image.forUser(user, {
+							 				width: 40,
+							 				height: 40,
+							 				placeholder: 'user',
+							 				'link': 'spotify:app:bungalow:user:' + user.username
+							 				
+							 			}); 
+
+  document.body.appendChild(image.node);
+  image.node.style.cssFloat = 'left';
+  image.node.style.marginRight = '3px';
+							 			var popup = Popup.withText(user.username);
+							 			console.log(image.node);
+									    image.node.addEventListener('mouseover', showPopup, false);
+									    image.node.addEventListener('mouseout', hidePopup, false);
+									    function showPopup() {
+									    	// Set the text that is found in the target's data-tooltip attribute
+									   		 // This is just an example, and you can get the text from anywhere
+											// popup.setText(this.getAttribute('data-tooltip'));
+
+										 	// Show the popup for this target element
+										    popup.showFor(this);
+									 	}
+									  	function hidePopup() {
+									  	  	popup.hide();
+									 	 }
+							 			document.querySelector('#embassadours').appendChild(image.node);
+
+							 			// add to admin
+							 			if (isAdmin) {
+							 				require(['$views/buttons'], function (buttons) {
+							 					console.log('f');
+							 					var item = document.createElement('tr');
+							 					
+							 					var button = buttons.Button.withLabel('Increase reputation');
+							 					var image = Image.forUser(user, {
+									 				width: 90,
+									 				height: 90,
+									 				link: 'spotify:app:bungalow:user:' + user.username,
+							 						placeholder: 'user'
+									 			});
+									 			
+									 			var label = document.createElement('td');
+
+									 			
+									 			var td = document.createElement('td');
+									 			td.appendChild(image.node);
+									 			item.appendChild(label)
+									 			var n = document.createElement('span');
+									 			n.style.fontSize = '16px';
+									 			n.style.lineHeight = '32px';
+									 			n.innerText = user.username;
+									 			//label.appendChild(n);
+									 			var td2 = document.createElement('td');
+
+									 	//		
+									 			
+									 			item.style.width = '100%';
+									 			var td3 = document.createElement('td');
+
+									 			var btnSubscribe = buttons.SubscribeButton.forUser(user);;
+									 			td3.appendChild(n)
+									 			td3.appendChild(document.createElement('br'));
+									 			td3.appendChild(btnSubscribe.node);
+									 			
+									 			td3.appendChild(document.createElement('br'));
+									 			td2.appendChild(button.node);
+									 			td3.setAttribute('valign', 'top');
+									 			item.appendChild(td);
+									 			item.appendChild(td3);
+									 			item.appendChild(td2);
+
+									 			document.querySelector('#admin #embassadours').appendChild(item);
+									 			document.querySelector('#admin #embassadours').appendChild(document.createElement('br'));
+
+							 				});
+							 			}
+							 		});
+						 		});
+						 	});
+					 	}).fail(function (users) {
+
+					 	});
+					 });
 				 });
-			 });
-		} else {
-			document.querySelector('#embdetails').style.display = 'block';
+			} else {
+				document.querySelector('#embdetails').style.display = 'block';
 
-			// Get artists associated with the account
-			var xmlHttp = new XMLHttpRequest();
-			console.log(user);
-			xmlHttp.open('GET', 'http://home:8080/myproject/bungalow/api/v1_api/user/?format=json&user=' + user.username, true);
-			xmlHttp.onreadystatechange = function () { 
-				if(xmlHttp.readyState == 4) {
-					if(xmlHttp.status == 200) {
-						require(['$api/models', '$views/image#Image', '$views/buttons'], function (models, image, buttons) {
-							new models.Session().user.load('username').done(function (user2) {
-								if(user.username != user2.username) {
-				 					
-				 					var button = buttons.Button.withLabel('Invite to be embassadour');
-				 					document.querySelector('#role').appendChild(button.node)
-				 				}
-				 			});
-							var data = eval('(' + xmlHttp.responseText + ')');
-							var collection = new context.EmbassadourCollection();
-							data.objects.forEach(function (user) {
-								collection.items.push('spotify:artist:' + user.identifier);
-							});
-							console.log(collection);
-							var promises = [];
-							collection.items.forEach(function (item) {
-								promises.push(models.Artist.fromURI(item).load('image', 'name'));
-							});
-							var artists = [];
-							var promise = models.Promise.join(promises).each(function (artist) {
-								artists.append(artist);
-							}).done(function (artists_) {
-								document.querySelector('#artistdetails').style.display = 'block';
-								document.querySelector('#artistdetails').innerHTML = '<b>Embassadour for</b><ul class=\"flow\" id=\"artistflow\"></ul>';
-
-								artists.forEach(function (artist) {
-									var image = Image.forArtist(artist, {
-										width: 40,
-										height: 40,
-										placeholder: 'artist'
-									});
-									var popup = Popup.withText(artist.name);
-						 			console.log(image.node);
-								    image.node.addEventListener('mouseover', showPopup, false);
-								    image.node.addEventListener('mouseout', hidePopup, false);
-								    document.querySelector('artistflow').appendChild(image.node);
-								    function showPopup() {
-								    	// Set the text that is found in the target's data-tooltip attribute
-								   		 // This is just an example, and you can get the text from anywhere
-										// popup.setText(this.getAttribute('data-tooltip'));
-
-									 	// Show the popup for this target element
-									    popup.showFor(this);
-								 	}
-								  	function hidePopup() {
-								  	  	popup.hide();
-								 	}
+				// Get artists associated with the account
+				var xmlHttp = new XMLHttpRequest();
+				console.log(user);
+				xmlHttp.open('GET', 'http://home:8080/myproject/bungalow/api/v1_api/user/?format=json&user=' + user.username, true);
+				xmlHttp.onreadystatechange = function () { 
+					if(xmlHttp.readyState == 4) {
+						if(xmlHttp.status == 200) {
+							require(['$api/models', '$views/image#Image', '$views/buttons'], function (models, image, buttons) {
+								new models.Session().user.load('username').done(function (user2) {
+									if(user.username != user2.username) {
+					 					
+					 					var button = buttons.Button.withLabel('Invite to be embassadour');
+					 					document.querySelector('#role').appendChild(button.node)
+					 				}
+					 			});
+								var data = eval('(' + xmlHttp.responseText + ')');
+								var collection = new context.EmbassadourCollection();
+								data.objects.forEach(function (user) {
+									collection.items.push('spotify:artist:' + user.identifier);
 								});
+								console.log(collection);
+								var promises = [];
+								collection.items.forEach(function (item) {
+									promises.push(models.Artist.fromURI(item).load('image', 'name'));
+								});
+								var artists = [];
+								var promise = models.Promise.join(promises).each(function (artist) {
+									artists.append(artist);
+								}).done(function (artists_) {
+									document.querySelector('#artistdetails').style.display = 'block';
+									document.querySelector('#artistdetails').innerHTML = '<b>Embassadour for</b><ul class=\"flow\" id=\"artistflow\"></ul>';
 
+									artists.forEach(function (artist) {
+										var image = Image.forArtist(artist, {
+											width: 40,
+											height: 40,
+											placeholder: 'artist',
+											link: artist.uri
+										});
+										var popup = Popup.withText(artist.name);
+							 			console.log(image.node);
+							 			image.node.style.cssFloat = 'right';
+									    image.node.addEventListener('mouseover', showPopup, false);
+									    image.node.addEventListener('mouseout', hidePopup, false);
+									    document.querySelector('artistflow').appendChild(image.node);
+									    function showPopup() {
+									    	// Set the text that is found in the target's data-tooltip attribute
+									   		 // This is just an example, and you can get the text from anywhere
+											// popup.setText(this.getAttribute('data-tooltip'));
+
+										 	// Show the popup for this target element
+										    popup.showFor(this);
+									 	}
+									  	function hidePopup() {
+									  	  	popup.hide();
+									 	}
+									});
+
+								});
 							});
-						});
+						}
 					}
-				}
-			};
-			xmlHttp.send(null);
-			document.querySelector("#username").innerText = user.name;
-		}
-		
-		
+				};
+				xmlHttp.send(null);
+				document.querySelector("#username").innerText = user.name;
+			}
+			
+		});
 
 	}
 	var bungalow = new context.Bungalow();
